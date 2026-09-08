1bd6a8d082fcbd506ae093f8d699c0a1b21290d3
ajanmaloku11 <ajan.maloku11@gmail.com>
2026-09-08 20:46:19 +0200

    new

---
+++ moduli 26/js/main.js
@@ -0,0 +1,46 @@
+console.error("THErE IS AN ERROR IN THE CODE!!!")
+
+// VARIABLA
+
+var x = 10;
+
+//var *ds = 10;
+
+var a = 50 + 50;
+console.log(a);
+
+var b = 20;
+var a = 30;
+var d = b+c;
+console.log(d);
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var e = 100;
+var f = 5;
+var g = h / i;
+console.log(j);
+
+var e = 50;
+var f = 4;
+var g = k % l;
+console.log(m);
+
+var n = 99;
+n++;
+console.log(n);
+
+var o =101;
+o--;
+console.log(o);
+
+var x = 15;
+x+=5;
+console.log(x);
+
+var r = 35;
+r-=5;
+console.log(r);
